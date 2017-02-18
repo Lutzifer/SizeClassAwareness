8e64d79458137d6b38bcfc3edff642d8bfc356b3
Wolfgang Lutz <wlut@num42.de>
2017-02-18 16:17:03 +0100

    naming

--- TraitAwareVC-Demo/TraitAwareVC-Demo/UIViewController+TraitAwareness.swift
+++ TraitAwareVC-Demo/TraitAwareVC-Demo/UIViewController+TraitAwareness.swift
@@ -1,5 +1,5 @@
 //
-//  TraitAwareUIViewController.swift
+//  UIViewController+TraitAwareness.swift
 //
 //  Created by Wolfgang Lutz on 12.02.17.
 //  Copyright © 2017 Wolfgang Lutz. All rights reserved.
